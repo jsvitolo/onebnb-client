403ff9ab27731af1f51af65003301f856dddf20b
Jones Vitolo <jsvitolo@gmail.com>
2017-02-04 16:12:20 -0200

    Added in the router and in the app.module the new components

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './users/register/register.component';
 import { LoginComponent } from './users/login/login.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { Angular2TokenService } from 'angular2-token';
 
 // Cria nossas rotas
 const appRoutes: Routes = [
     { path: '', pathMatch: 'full', component: HomeComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'user/edit', component: UserEditComponent, canActivate: [Angular2TokenService] }
 ];
 
 // Exporte a constante routinng para importamos ela no arquivo app.modules.ts
